57a8fa00a0ae90dc6f2a8f4ac334631655649509
ahmedlajalati-sys <ahmedlajalati@gmail.com>
2026-06-12 22:57:45 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,17 +2,44 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static('.'));
+// Serve all static files from current directory
+app.use(express.static(__dirname));
 
+// Routes
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'login.html'));
+});
+
+app.get('/index.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/login.html', (req, res) => {
     res.sendFile(path.join(__dirname, 'login.html'));
 });
 
+app.get('/profile.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'profile.html'));
+});
+
+app.get('/reels.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'reels.html'));
+});
+
+app.get('/settings.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'settings.html'));
+});
+
+app.get('/messages.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'messages.html'));
+});
+
+// Catch all other routes
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'login.html'));
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`✅ Server running on http://localhost:${PORT}`);
 });
